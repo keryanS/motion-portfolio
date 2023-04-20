d964e9e45abdb02f3b9883483e83ddb09a15649f
keryan <keryan.sanie@gmail.com>
2023-04-20 16:46:35 +0200

    fix: doc

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -10,16 +10,16 @@ const config = {
   tagline: 'Documentation développeur',
   favicon: 'img/favicon.ico',
   deploymentBranch: 'gh-pages',
-
+  trailingSlash: false,
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://keryans.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dsfr/',
+  baseUrl: '/motion-portfolio/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'keryanS', // Usually your GitHub org/user name.
+  organizationName: 'keryans', // Usually your GitHub org/user name.
   projectName: 'motion-portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -41,13 +41,6 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           includeCurrentVersion: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
