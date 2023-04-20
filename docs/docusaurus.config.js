9f8fe1f378ecc4c81d2fe670ea1b772177cda975
keryan <keryan.sanie@gmail.com>
2023-04-20 16:48:11 +0200

    test

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/dsfr/',
 
-  // GitHub pages deployment config.
+  // GitHub pages  deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'keryanS', // Usually your GitHub org/user name.
   projectName: 'motion-portfolio', // Usually your repo name.
